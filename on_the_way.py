b0e449e9bb2bd7e16b2b2e43738a63765ba044d2
Robin Neufeld <15841703+metavee@users.noreply.github.com>
2018-06-28 18:23:48 -0400

    Changed method to deal with multiple addresses, but needs manual batching.

--- on_the_way.py
+++ on_the_way.py
@@ -1,6 +1,9 @@
 import os
+import numpy as np
 import pandas as pd
 import sys
+import warnings
+import traceback
 
 import googlemaps
 import tqdm
@@ -10,6 +13,7 @@ import tqdm
 filename = 'doctors.csv'
 home_addr = 'Conestoga Mall, Waterloo, ON'
 work_addr = 'Davis Centre, University of Waterloo, Waterloo, ON'
+max_rows = 20 # limit requests while testing
 
 
 secret_api_key = os.environ.get('GMAPS_API_KEY', '')
@@ -18,23 +22,36 @@ gmaps = googlemaps.Client(key=secret_api_key)
 spreadsheet = pd.read_csv(filename)
 
 # TODO: batch API calls
-def distance_between(from_addr, to_addr):
-    data = gmaps.distance_matrix([from_addr], [to_addr])
-    dist = data['rows'][0]['elements'][0]['distance']['value']
+def distance_between(endpoint_addrs, midpoint_addrs):
+    data = gmaps.distance_matrix(endpoint_addrs, midpoint_addrs)
 
-    return dist
+    results = np.zeros((len(endpoint_addrs), len(midpoint_addrs)))
+    results[:, :] = np.nan
+
+    if 'status' not in data or data['status'] != 'OK':
+        warnings.warn('API call failed.')
+        return results
+
+    for i, endpoint_addr in enumerate(endpoint_addrs):
+        for j, midpoint_addr in enumerate(midpoint_addrs):
+            try:
+                if np.random.random() < 0.05:
+                    continue
+                results[i, j] = data['rows'][i]['elements'][j]['distance']['value']
+            except (KeyError, TypeError) as exc:
+                warnings.warn('Could not find distance between `{0}` and `{1}`.'.format(endpoint_addr, midpoint_addr))
+                traceback.print_exc()
+
+    return results.transpose()
 
 # build a list of distance from home/work to each place
-distances_from_home = []
-distances_from_work = []
-
-for addr in tqdm.tqdm(spreadsheet['address'].values):
-    distances_from_home.append(distance_between(home_addr, addr))
-    distances_from_work.append(distance_between(work_addr, addr))
+distances = distance_between([home_addr, work_addr], spreadsheet.address.values[:max_rows])
 
 # copy the lists into table
-spreadsheet['distance from home'] = distances_from_home
-spreadsheet['distance from work'] = distances_from_work
+spreadsheet['distance from home'] = np.nan
+spreadsheet['distance from work'] = np.nan
+spreadsheet['distance from home'].iloc[:max_rows] = distances[:, 0]
+spreadsheet['distance from work'].iloc[:max_rows] = distances[:, 1]
 
 # add a column for the combined distance
 spreadsheet['combined distance'] = spreadsheet['distance from home'] + spreadsheet['distance from work']
